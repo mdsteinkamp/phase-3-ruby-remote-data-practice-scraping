d85428aa48d7ad3def260d7e9221a8e488628409
Matthew Steinkamp <mdsteinkamp@gmail.com>
2023-06-10 18:05:23 -0700

    commit tree 47837998cdefcaa1dfead7a7cb5426e199c1e915

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,15 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
+# html = open("https://flatironschool.com/")
+
+doc = Nokogiri::HTML(URI.open("http://flatironschool.com/"))
+
+info = doc.css(".heading-50-black color-black")
+
+info.each do |thing|
+  puts thing.text.strip
+end
+
+
+
